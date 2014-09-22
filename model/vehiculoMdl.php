ec4e6c4205422910d9e309ac3ab9a908f59aa421
paxxita <irenapaz@yahoo.com>
2014-09-21 20:02:32 -0500

    modificar vehiculoMdl.php funcion modificar para q reciva como parametro el vin del auto a modificar

--- model/vehiculoMdl.php
+++ model/vehiculoMdl.php
@@ -18,16 +18,18 @@
 			return array("carro1","carro2","carro3","carro4");
 		}
 
-		public function modificar()
+		public function modificar($vin)
 		{
-			#hace un update de la BD.
+			#modificar el auto con ese vin
+			#se puede hacer uso de $$var(variables variables) 
+			#para saber que campo se va a modificar y x cual valor
 			return TRUE;
 		}
 
 		public function eliminar($vin)
 		{
 			#cambiar status del vehículo.
-			#vin.status = false;
+			#vehiculo.status = false;
 			#update en BD.
 			return TRUE;
 		}
