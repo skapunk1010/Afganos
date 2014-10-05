a3b9454f2c3f9cf5314e327db90b080d59dfa870
paxxita <irenapaz@yahoo.com>
2014-10-05 10:47:49 -0500

    metodo de insercion a la base de datos, recibiendo area, encargado y descripcion, modificaciones al constructor de la clase area.php

--- model/Area.php
+++ model/Area.php
@@ -9,73 +9,64 @@
 
 		/**
 		*Constructor
+		*@param String $Encargado_Codigo
+		*@param String $area
 		*/
-		function __construct($idArea, $Encargado_Codigo, $area){
+		function __construct($Encargado_Codigo, $area){
 
-			$this -> idArea = $idArea;
 			$this -> Encargado_Codigo = $Encargado_Codigo;
 			$this -> area = $area;
 		}
 
 
 		/**
-		*@return String Regresa nueva área
+		*@return String, Regresa nombre área
 		*/
 		public function getArea(){
 			return $this -> area;
 		}
-
 		/**
-		*@return String Regresa idArea
+		*@return Int, Regresa idArea
 		*/
 		public function getIdArea(){
 			return $this -> idArea;
 		}
-
 		/**
-		*@return String Regresa al encargado
+		*@return String, Regresa el codigo del encargado
 		*/
 		public function getEncargado(){
 			return $this -> Encargado_Codigo;
 		}
-
-
 		/**
-		*@return String Regresa descripción
+		*@return String, Regresa una descripcion del area
 		*/
 		public function getDescripcion(){
 			return $this -> descripcion;
 		}
 
 		/**
-		*@param String $area recibe el nombre de área
+		*@param String $area, recibe el nombre de área
 		*/
 		public function setArea($area){
 			return $this -> area = $area;
 		}
-
 		/**
-		*@param String $idArea recibe el id de área
+		*@param Int $idArea, recibe el id de área
 		*/
 		public function setIdArea($idArea){
 			return $this -> idArea = $idArea;
 		}
-
 		/**
 		*@param String $Encargado_Codigo recibe el codigo de empleado
 		*/
 		public function setEncargado($Encargado_Codigo){
 			return $this -> Encargado_Codigo = $Encargado_Codigo;
 		}
-
-
 		/**
-		*@param String $descripcion recibe el nombre de una ubicación
+		*@param String $descripcion, recibe una descripcion del area
 		*/
 		public function setDescripcion($descripcion){
 			return $this -> descripcion = $descripcion;
 		}
 	}
-
-
 ?>
