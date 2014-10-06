911daea478159273eb573a07214bef5e7cc4ff3f
Argelia <argelia.garcia.g@gmail.com>
2014-10-05 22:38:44 -0500

    Se crea clase marcaCtrl.php

---
+++ controller/marcaCtrl.php
@@ -0,0 +1,98 @@
+<?php
+
+    class marcaMdl{
+
+        private $modelo;
+        
+        /**
+         *Crea la instancia de un objeto de la clase marcaMdl. 
+         */
+        function __construct(){
+
+            require('model/marcaMdl.php');
+            $this -> modelo = new marcaMdl();
+        }
+        
+
+        /**
+        *Se recibe la acción a realizar con el modelo.
+         */
+        public function run(){
+
+            switch($_REQUEST['accion']){
+                
+                case 'insertar': 
+                    $this -> insertar();
+                    break;
+
+                case 'consultar':
+                    $this -> consultar();
+                    break;
+
+                case 'modificar':
+                    $this -> modificar();
+                    break;
+                        
+                default: 
+                    require('view/error.php');
+            }
+        }
+        
+        /**
+         *Inserta una nueva marca de auto.
+         */
+        public function insertar(){
+        
+            require('controller/validadorCtrl.php');
+            $marca = $_REQUEST['marca'];
+            if(validadorCtrl::validarTexto($marca)){
+                $resultado = $this -> modelo -> insertar($marca); 
+                if($resultado)
+                    require('view/marcaInsertar.php');
+                else
+                    require('view/errorMarcaInsertar.php');
+            }
+            else{
+                die('Cadena de marca inválida');
+            }
+        }
+
+        /**
+         * Hace la consultar de marcas.
+         */
+
+        public function consultar(){
+            require('controller/validadorCtrl.php');
+            $marca = $_REQUEST['marca'];
+            if(validadorCtrl::validarTexto($marca)){
+                $resultado = $this -> modelo -> consultar($marca); 
+                if($resultado)
+                    #var_dump($resultado);
+                else
+                    require('view/errorMarcaConsulta.php');
+            }
+            else{
+                die('Cadena de marca inválida');
+            }  
+        }
+        
+        /**
+         * Modifica algún nombre de marca.
+         */
+        public function modificar(){
+            require('controller/validadorCtrl.php');
+            $marca = $_REQUEST['marca'];
+            if(validadorCtrl::validarTexto($marca)){
+                $resultado = $this -> modelo -> modificar($marca); 
+                if($resultado)
+                    require('view/marcaModificar.php');
+                else
+                    require('view/errorMarcaModificar.php');
+            }
+            else{
+                die('Cadena de marca inválida');
+            }
+        }
+    }
+
+?>
