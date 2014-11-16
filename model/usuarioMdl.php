3cfc8d235de101eb3a39c2c0b92507ca708ca64b
Argelia <argelia.garcia.g@gmail.com>
2014-11-16 11:11:58 -0600

    Se completa la función de insertar usuario

--- model/usuarioMdl.php
+++ model/usuarioMdl.php
@@ -18,31 +18,16 @@
 		*@param String $email recibe una cadena de email nuevo
 		*@return bool dependiendo de su validez.
 		*/
-		public function insertar($codigo, $nombre, $apellido, $telefono, $email){
-			#Generar codigoUsuario
-			$queryParaUsuario 	= "SELECT COUNT() as total FROM Usuario";
-			$resultado			= $this->conexion->query($queryParaUsuario);
-			$row				= $resultado->fetch_array();
-			$nUsuarios 			= $row[0] + 1;
-			$codigo 			= str_pad($nUsuarios, 6, '0', STR_PAD_LEFT);
-
-			$usuario = new Usuario($codigo, $nombre, $apellido, $telefono, $email);
-			#simular conexion a la base de datos
+		public function insertar($codigo, $usuario, $contrasenha, $privilegios){
+			$fecha = date('Y-m-d H:i:s');
+			$status = "1";
+			
+			$queryInsertar = "INSERT INTO Usuario (Codigo, usuario, contrasenha, status, fechaRegistro, privilegios) 
+			VALUES ('".$codigo."','".$usuario."','".$contrasenha."','".$status."','".$fecha."','".$privilegios."')";
+			$insercion = $this->conexion->query($queryInsertar);
 			$this -> conexion -> close();
-			return TRUE;
+			return $insercion;
 		}
-
-		/**
-		*@param String $codigo recibe el código del usuario a modificar.
-		*@return bool según sea su validez.
-		*/
-		/*public function modificar($codigo)
-		{
-			#modificar usuarios a traves de su codigo
-			#hacer uso de variables variables como en vehiculoMdl.php para saber los 
-			#campo a modificar
-			return TRUE;
-		}*/
 
 		/**
 		*@param String $codigo recibe el código del usuario a deshabilitar.
@@ -55,6 +40,10 @@
 			return $correcto;
 		}
 
+		/**
+		*@param String $codigo recibe el código del usuario a habilitar.
+		*@return bool según sea su validez.
+		*/
 		public function habilitar($codigo){
 			$query = "UPDATE Usuario SET status = '1' WHERE Codigo = '".$codigo."'";
 			$correcto = $this->conexion->query($query);
@@ -62,6 +51,10 @@
 			return $correcto;
 		}
 
+		/**
+		*@param String $codigo recibe el código del usuario y $passAct que es la contraseña actual.
+		*@return bool según sea su validez.
+		*/
 		public function existeUsuario($codigo,$passAct){
 			$query = "SELECT * FROM Usuario WHERE Codigo = '".$codigo."' AND contrasenha = '".$passAct."'";
 			$existe = $this->conexion->query($query);
@@ -79,14 +72,36 @@
 			return $array;
 		}
 
+		/**
+		*@param String $codigo recibe el código y $passNew para cambiar la contraseña
+		*@return bool según sea su validez.
+		*/
 		public function cambiarContrasenha($codigo,$passNew)
 		{
-	
 			$consulta = "UPDATE Usuario SET contrasenha = '".$passNew."' WHERE Codigo = '".$codigo."'";
 			$resultado = $this->conexion->query($consulta);
 			$this -> conexion -> close();
 			return $resultado;
-			
+		}
+
+		/**
+		*@return array con los usuarios registrados.
+		*/
+		public function consultar(){
+			$query = "SELECT * FROM Usuario";
+			$resultado = $this->conexion->query($query);
+			$array = array();
+	
+			if($resultado){
+				$i = 0;
+				while($fila = $resultado->fetch_assoc()){
+					$array[$i++] = $fila;
+				}
+			}
+			else {
+				$array = NULL;
+			}
+			return $array;
 		}
 	}
 ?>
