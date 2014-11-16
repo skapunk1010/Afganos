53207b6b9f9c16beb39698f95fced3e1d9f7e3a6
Argelia <argelia.garcia.g@gmail.com>
2014-11-16 01:43:09 -0600

    Se completa el modelo las áreas

--- model/areaMdl.php
+++ model/areaMdl.php
@@ -21,56 +21,71 @@
          *@return bool TRUE si la inserción a la BD fue satisfactoria.
 		 */
 		public function insertar($Encargado_Cod, $Area, $descripcion){
-			
-			$nuevaArea = new Area($Encargado_Cod, $Area);
-			
 			$areaESC = $this -> conexion -> real_escape_string($Area);
 			$descripcionESC = $this -> conexion -> real_escape_string($descripcion);
-
 			$query = "INSERT INTO Area (Encargado_Codigo, area, descripcion) 
-				VALUES ('".$Encargado_Cod."','".$areaESC."','".$descripcionESC."')";
-			$correcto = $this -> conexion -> query($query);
-
-			if($correcto)
-				$nuevaArea -> setIdArea($this -> conexion -> insert_id);
-			else $nuevaArea = NULL;
+			VALUES ('".$Encargado_Cod."','".$areaESC."','".$descripcionESC."')";
 			
+			$correcto = $this -> conexion -> query($query);
 			$this -> conexion -> close();
 			return $correcto; 
 		}
 
 		/**
 		 *Consulta a la base de datos del área especificada x el IdArea
-		 *@param int $Idarea, PK del area a consultar.
-		 *
-		 *@return bool TRUE si la consulta fue satisfactoria.
+		 *@return array con listado de áreas o NULL si no hay áreas que mostrar.
 		 */
-		public function buscar($IdArea){
+		public function listar(){
 			
-			$query = "SELECT * FROM Area WHERE IdArea = ".$IdArea;
-			
+			$query = "SELECT * FROM Area";			
 			$correcto = $this -> conexion -> query($query);
+			$array = array();
+			if($correcto){
+				$i = 0;
 
-			if($correcto != FALSE){
-				
-				### se encontro el area 
+				while ($fila = $correcto->fetch_assoc()) {
+        			$array[$i++] = $fila;
+  			  }
 			}
-
+			else $array = NULL;
 			$this -> conexion -> close();
-			return $correcto; 
+			return $array; 
 		}
 
 		/**
+		 * Hace la consulta específica de un área
+		 *@param Int $idArea
+         *@return array con los datos del área consultada. NULL en caso de no existir.
+		 */
+		public function consultar($idArea){
+			$query = "SELECT * FROM Area WHERE idArea = '".$idArea."'";
+			$resultado = $this -> conexion -> query($query);
+			$array = array();
+			if($resultado){
+				$i = 0;
+
+				while ($fila = $resultado->fetch_assoc()) {
+        			$array[$i++] = $fila;
+  			  }
+			}
+			else $array = NULL;
+			$this -> conexion -> close();
+			return $array; 
+		}
+
+
+
+		/**
 		 * Hace la modificación a la base de datos del área indicada.
-		 *@param Int $IdArea
+		 *@param Int $idArea
          *@param campo, variable variable, en su 1 valor campo a modificar, en su 2 valor: el nuevo valor
          *
          *@return bool TRUE si la modificación fue satisfactoria.
 		 */
-		public function modificar($IdArea, $campo){
-			
-			#Mostrar resultados
-			return TRUE; #Retorno temporal
+		public function modificar($idArea, $campo,$nuevoCampo){
+			$query = "UPDATE Area SET ".$campo." = '".$nuevoCampo."' WHERE idArea = '".$idArea."'";
+			$resultado = $this -> conexion ->query($query);
+			return $resultado;
 		}
 	}
 ?>
