5266b0d1c82afd2355269fb89906d8f185420de3
Erick Martinez <s_e_b_a_s_1010@hotmail.com>
2014-11-26 21:30:32 -0600

    Ajax en Area

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -10,27 +10,71 @@ $(document).ready(function(){
     var windowheight = $(window).height();
     $('.main').css('min-height', windowheight - headerheight - footerheight-20);
     $("[class='checkboxSwitch']").bootstrapSwitch();
+
+    $('#codigoEncargado').blur(function(){
+        var codigoEmpleado = $(this).val();
+        $.ajax({
+                type: 'POST',
+                data: {codigoEmpleado:codigoEmpleado},
+                url: 'index.php?ctrl=empleado&accion=consultarAjax',
+                dataType: 'json',
+                success: function(json){
+                    console.log(json);
+                    var nombre = json[0].nombre + " ";
+                    nombre += json[0].apellidoMaterno + " ";
+                    nombre += json[0].apellidoPaterno;
+                    $('#nombreEncargado').val(nombre);
+                },
+                error:function(json){
+                    $('#nombreEncargado').val('');
+                    //Mensaje no se encontraron ubicaciones para dicha sección
+                }
+        });
+    });
+
+    $('#aceptarAsignacion').click(function(){
+        var idUbicacion = $('#numeroUbicacion').val();
+        var idArea      = getVariableUrl('id');
+        if(idUbicacion !== null){
+            $.ajax({
+                type: 'POST',
+                data: {idArea:idArea,idUbicacionid:idUbicacion},
+                url: 'index.php?ctrl=ubicacion&accion=insertar',
+                dataType: 'json',
+                success: function(json){
+                    console.log(json);
+                    alert('ubicacion asignada');
+                },
+                error:function(json){
+                    //Mensaje no se encontraron ubicaciones para dicha sección
+                    console.log(json);
+                    alert('no se pudo asignar ubicacion');
+                }
+            });
+        }
+    });
 });
 
 function actualizarNumeroUbicacion(dropSeccion){
     var valor = dropSeccion.value ;
-    if(valor !== "0"){
-        alert(valor);
+    if(valor !== "0"){     
+        /*$.post("index.php?ctrl=ubicacion&accion=buscar",function(data,status){
+          alert("Data: " + data + "\nStatus: " + status);
+        });   */
         $.ajax({
                 type: 'POST',
                 data: {seccion:valor},
                 url: 'index.php?ctrl=ubicacion&accion=buscar',
-                //url: 'view/areaConsultar.html',
                 dataType: 'json',
                 success: function(json){
-                    console.log(json);
+                    $('#numeroUbicacion').empty();
+                    for(i in json){
+                        $('#numeroUbicacion').append('<option value='+json[i].idUbicacion+'>'+json[i].numero+'</option>');
+                    }
                 },
                 error:function(json){
-                    //alert('falló');
-                    for(i in json){
-                        console.log(json[i]);
-                    }
-
+                    $('#numeroUbicacion').empty();
+                    //Mensaje no se encontraron ubicaciones para dicha sección
                 }
         });
     }
